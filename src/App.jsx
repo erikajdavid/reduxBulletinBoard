3a3467fff9e32f30972866344f011bbd89c557fc
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-12-20 12:27:09 -0500

    created and imported PostsList component, updated files from .js to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import PostsList from './features/posts/PostsList'
 
 function App() {
 
   return (
     <main>
       <h1>Bulletin Board</h1>
+      <PostsList />
     </main>
   )
 }
