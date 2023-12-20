2f4fe8d0ff4a9196e4de7054915f00598d9f5cfa
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-12-20 12:32:09 -0500

    fixed renderedPosts bug

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -7,10 +7,12 @@ const PostsList = () => {
     const posts = useSelector(selectAllPosts)
 
     const renderedPosts = posts.map(post => {
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-        </article>
+        return(
+            <article key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.content.substring(0, 100)}</p>
+            </article>          
+        )
     })
     
     return (
