00ea8426684bbb8e512dd964ea963bae4a3ff32d
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2023-12-29 15:37:23 -0500

    created fetchUsers async thunk

--- src/features/users/usersSlice.jsx
+++ src/features/users/usersSlice.jsx
@@ -1,24 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-    {
-        id: '0',
-        name: 'Jessica',
-    },
-    {
-        id: '1',
-        name: 'Matt',
-    },
-    {
-        id: '3',
-        name: 'Linda',
-    },
-]
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+// const initialState = [
+//     {
+//         id: '0',
+//         name: 'Jessica',
+//     },
+//     {
+//         id: '1',
+//         name: 'Matt',
+//     },
+//     {
+//         id: '3',
+//         name: 'Linda',
+//     },
+// ]
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    try {
+        const response = await axios.get(USERS_URL)
+        return response.data
+    } catch (err) {
+        return err.message;
+    }
+})
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload;
+        })
+    }
 })
 
 export const selectAllUsers = (state) => state.users
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice'
+
+//we want the users immediately when the application loads
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
